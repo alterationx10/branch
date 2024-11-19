e0ab134542c5e7414900e42e6f6458fb9ff9e826
Mark Rudolph <mark@k8ty.app>
2024-11-18 21:11:07 -0500

    fix classtag issue

--- keanu/src/main/scala/dev/wishingtree/branch/keanu/actors/ActorContext.scala
+++ keanu/src/main/scala/dev/wishingtree/branch/keanu/actors/ActorContext.scala
@@ -5,7 +5,7 @@ import scala.reflect.{ClassTag, classTag}
 trait ActorContext[A <: Actor: ClassTag] {
   private[actors] val ctorArgs: Seq[Any]
   private[actors] val identifier  =
-    classTag[A].getClass.getCanonicalName
+    classTag[A].runtimeClass.getCanonicalName
   private[actors] def create(): A = {
     try {
       classTag[A].runtimeClass.getConstructors.head
--- keanu/src/main/scala/dev/wishingtree/branch/keanu/actors/ActorRefId.scala
+++ keanu/src/main/scala/dev/wishingtree/branch/keanu/actors/ActorRefId.scala
@@ -8,7 +8,7 @@ private [actors] case class ActorRefId(name: String, propId: String) {
 
 private [actors] object ActorRefId {
   def apply[A <: Actor: ClassTag](name: String): ActorRefId =
-    ActorRefId(name, classTag[A].getClass.getCanonicalName)
+    ActorRefId(name, classTag[A].runtimeClass.getCanonicalName)
     
   def fromIdentifier(id: String): ActorRefId = {
     val (name: String) :: (propId: String) :: Nil = id.split(":").toList: @unchecked
