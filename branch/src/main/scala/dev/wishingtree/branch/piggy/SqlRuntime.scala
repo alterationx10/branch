2517be0088ada2064b958b85e18a0affe28c98ee
Mark Rudolph <mark@k8ty.app>
2024-11-23 21:48:09 -0500

    update SqlRuntime to use 1 conn per execute + add txn/rollbacks

--- branch/src/main/scala/dev/wishingtree/branch/piggy/SqlRuntime.scala
+++ branch/src/main/scala/dev/wishingtree/branch/piggy/SqlRuntime.scala
@@ -7,7 +7,6 @@ import java.sql.{Connection, PreparedStatement}
 import java.util.concurrent.{CompletableFuture, ExecutorService}
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.*
-import scala.jdk.FutureConverters.*
 
 trait SqlRuntime {
   def execute[A, B <: ResourcePool[Connection]](sql: Sql[A])(using
@@ -29,88 +28,93 @@ object SqlRuntime extends SqlRuntime {
 
   override def execute[A, B <: ResourcePool[Connection]](sql: Sql[A])(using
       pool: B
-  ): Try[A] =
-    eval(sql)(using pool).get()
+  ): Try[A] = {
+    Try {
+      pool.use { conn =>
+        try {
+          conn.setAutoCommit(false)
+          val result = eval(sql)(using conn).get()
+          conn.commit()
+          result
+        } catch {
+          case e: Throwable => {
+            conn.rollback()
+            throw e
+          }
+        } finally {
+          conn.setAutoCommit(true)
+        }
+      }
+    }
+  }.flatten
 
   override def executeAsync[A, B <: ResourcePool[Connection]](sql: Sql[A])(using
       pool: B
-  ): Future[A] =
-    eval(sql).asScala.flatMap(t => Future.fromTry(t))(executionContext)
+  ): Future[A] = Future.fromTry(execute(sql))
 
-  private def eval[A, B <: ResourcePool[Connection]](sql: Sql[A])(using
-      pool: B
+  private def eval[A](sql: Sql[A])(using
+      connection: Connection
   ): CompletableFuture[Try[A]] = {
     CompletableFuture.supplyAsync(
       () => {
         sql match {
-          case Sql.StatementRs(sql, fn)               =>
-            Try {
-              pool.use { conn =>
-                Using.Manager { use =>
-                  val statement = use(conn.createStatement())
-                  val res       = statement.execute(sql)
-                  val rs        = use(statement.getResultSet)
-                  fn(rs)
-                }
+          case Sql.StatementRs(sql, fn) =>
+            Using.Manager { use =>
+              val statement = use(connection.createStatement())
+              val res       = statement.execute(sql)
+              val rs        = use(statement.getResultSet)
+              fn(rs)
+            }
+
+          case Sql.StatementCount(sql) =>
+            Using.Manager { use =>
+              val statement = use(connection.createStatement())
+              val res       = statement.execute(sql)
+              statement.getUpdateCount
+            }
+
+          case Sql.PreparedExec(sqlFn, args) =>
+            Using.Manager { use =>
+              val helpers               = args.map(sqlFn)
+              val ps: PreparedStatement =
+                use(connection.prepareStatement(helpers.head.psStr))
+              helpers.foreach(_.setAndExecute(ps))
+            }
+
+          case Sql.PreparedUpdate(sqlFn, args) =>
+            Using.Manager { use =>
+              val helpers               = args.map(sqlFn)
+              val ps: PreparedStatement =
+                use(connection.prepareStatement(helpers.head.psStr))
+              val counts: Seq[Int]      = helpers.map(_.setAndExecuteUpdate(ps))
+              counts.foldLeft(0)(_ + _)
+            }
+
+          case Sql.PreparedQuery(sqlFn, rsFun, args) =>
+            Using.Manager { use =>
+              val helpers               = args.map(sqlFn)
+              val ps: PreparedStatement =
+                use(connection.prepareStatement(helpers.head.psStr))
+              helpers.flatMap { h =>
+                rsFun(h.setAndExecuteQuery(ps))
               }
-            }.flatten
-          case Sql.StatementCount(sql)                =>
-            Try {
-              pool.use { conn =>
-                Using.Manager { use =>
-                  val statement = use(conn.createStatement())
-                  val res       = statement.execute(sql)
-                  statement.getUpdateCount
-                }
-              }
-            }.flatten
-          case Sql.PreparedExec(sqlFn, args)          =>
-            Try {
-              pool.use { conn =>
-                Using.Manager { use =>
-                  val helpers               = args.map(sqlFn)
-                  val ps: PreparedStatement =
-                    use(conn.prepareStatement(helpers.head.psStr))
-                  helpers.foreach(_.setAndExecute(ps))
-                }
-              }
-            }.flatten
-          case Sql.PreparedUpdate(sqlFn, args)        =>
-            Try {
-              pool.use { conn =>
-                Using.Manager { use =>
-                  val helpers               = args.map(sqlFn)
-                  val ps: PreparedStatement =
-                    use(conn.prepareStatement(helpers.head.psStr))
-                  helpers.map(_.setAndExecuteUpdate(ps)).sum
-                }
-              }
-            }.flatten
-          case Sql.PreparedQuery(sqlFn, rsFun, args)  =>
-            Try {
-              pool.use { conn =>
-                Using.Manager { use =>
-                  val helpers               = args.map(sqlFn)
-                  val ps: PreparedStatement =
-                    use(conn.prepareStatement(helpers.head.psStr))
-                  helpers.flatMap { h =>
-                    rsFun(h.setAndExecuteQuery(ps))
-                  }
-                }
-              }
-            }.flatten
+
+            }
+
           case Sql.FlatMap(sql, f: (Any => Sql[Any])) =>
             eval(sql).get() match {
               case Success(a) => eval(f(a)).get()
               case Failure(e) => Failure(e)
             }
-          case Sql.Recover(sql, f)                    => {
+
+          case Sql.Recover(sql, f) => {
             eval(sql).get match {
               case Failure(e) => eval(f(e)).get
               case success    => success
             }
           }
-          case Sql.MappedValue(a)                     => {
+
+          case Sql.MappedValue(a) => {
             Try(a)
           }
         }
