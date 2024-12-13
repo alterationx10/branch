1031b6ea366047a1836e4640c199feb9c16200b8
Mark Rudolph <mark@k8ty.app>
2024-12-12 23:41:07 -0500

    update base app trait and remove bultin config for now

--- branch/src/main/scala/dev/wishingtree/branch/ursula/UrsulaApp.scala
+++ branch/src/main/scala/dev/wishingtree/branch/ursula/UrsulaApp.scala
@@ -1,24 +1,11 @@
 package dev.wishingtree.branch.ursula
 
+import dev.wishingtree.branch.lzy.Lazy
 import dev.wishingtree.branch.ursula.command.Command
+import dev.wishingtree.branch.ursula.command.builtin.HelpCommand
+import scala.util.*
 
 trait UrsulaApp {
-
-  /** A unique identifier, primarily used for the configuration directory. It
-    * should be unique per app; CLI apps with the same appIdentifier will end up
-    * sharing the same configuration.
-    */
-  val appIdentifier: String
-
-  /** The directory where the configuration file will be stored. Defaults to
-    * "~/.appIdentifier"
-    */
-  lazy val configDirectory: String =
-    s"${java.lang.System.getProperty("user.home")}/.$appIdentifier"
-
-  /** The name of the configuration file to be used. Defaults to "config.json"
-    */
-  lazy val configFile: String = "config.json"
 
   /** This setting determines whether the built in HelpCommand is the default
     * command. Defaults true, override to false if you want to use a different
@@ -31,83 +18,62 @@ trait UrsulaApp {
     */
   val commands: Seq[Command]
 
-//  private lazy val builtInCommands: Seq[Command] = Seq(
-//    HelpCommand(commands = commands :+ ConfigCommand, isDefault = defaultHelp),
-//    ConfigCommand
-//  )
-//
-//  private lazy val commandLayer: ZLayer[Any, Nothing, Seq[Command]] =
-//    ZLayer.succeed(builtInCommands ++ commands)
-//
-//  /** Given the injected Seq[Command], parse out a Map keyed by the Command
-//   * trigger. Warns if multiple commands use the same trigger.
-//   */
-//  private val commandMap: RIO[Seq[Command], Map[String, Command]] =
-//    for {
-//      map <- ZIO.serviceWith[Seq[Command]](_.groupBy(_.trigger))
-//      _   <- ZIO.foreachDiscard(map.filter(_._2.size > 1).toList) { kv =>
-//        ZIO.logWarning(s"""
-//                Multiple commands injected with the same trigger - using first found:
-//                  ${kv._1} =>
-//                    ${kv._2.map(_.getClass.getSimpleName).mkString(", ")}
-//                """)
-//      }
-//    } yield map.map { case (a, b) => (a, b.head) }
-//
-//  /** Given the injected Seq[Command], parse out the trigger key-words
-//   */
-//  private val triggerList: RIO[Seq[Command], Seq[String]] =
-//    ZIO.serviceWith[Seq[Command]](_.map(_.trigger))
-//
-//  /** Given the injected Seq[Command], find the one flagged as default (if
-//   * present). Warns if multiple Commands have been set as default.
-//   */
-//  private val findDefaultCommand: RIO[Seq[Command], Option[Command]] =
-//    for {
-//      default <- ZIO.serviceWith[Seq[Command]](_.filter(_.isDefaultCommand))
-//      _       <- ZIO.when(default.size > 1) {
-//        ZIO.logWarning(s"""
-//          Multiple commands injected with isDefaultCommand=true - using the first:
-//            ${default.map(_.getClass.getSimpleName).mkString(", ")}
-//          """)
-//      }
-//    } yield default.headOption
-//
-//  /** The "main program" of Ursula, which wires everything together, and runs
-//   * Commands based on the arguments passed in
-//   */
-//  private final val program: ZIO[
-//    Seq[Command] & CliConfig & ZIOAppArgs,
-//    Throwable,
-//    ExitCode
-//  ] =
-//    for {
-//      args       <- ZIOAppArgs.getArgs
-//      trigger     = args.headOption
-//      cmpMap     <- commandMap
-//      drop1Ref   <- Ref.make[Boolean](true)
-//      cmd        <-
-//        ZIO
-//          .fromOption(trigger.flatMap(t => cmpMap.get(t)))
-//          .catchAll(_ =>
-//            drop1Ref.set(false) *>
-//              findDefaultCommand.someOrFail(
-//                new Exception(
-//                  "Could not find command from argument, and no default command provided"
-//                )
-//              )
-//          )
-//      shouldDrop <- drop1Ref.get
-//      _          <- cmd
-//        .processedAction(if (shouldDrop) args.tail else args)
-//    } yield ExitCode.success
-//
-//  /** The entry point to the CLI, pre-wired
-//   */
-//  override final def run: ZIO[ZIOAppArgs & Scope, Any, Any] =
-//    program
-//      .provideSome[ZIOAppArgs & Scope](
-//        commandLayer ++ CliConfigLive.live(configDirectory, configFile)
-//      )
+  private lazy val builtInCommands: Seq[Command] = Seq(
+    HelpCommand(commands = commands, isDefault = defaultHelp)
+  )
+
+  private lazy val _allCommands = commands ++ builtInCommands
+
+  private lazy val commandMap: Map[String, Command] =
+    _allCommands.groupBy(_.trigger).map { case (k, v) =>
+      if v.size > 1 then {
+        println(
+          s"Multiple commands injected with the same trigger - using first found:"
+        )
+        v.foreach(c => println(c.getClass.getSimpleName))
+      }
+      k -> v.head
+    }
+
+  private lazy val triggerList: Seq[String] =
+    _allCommands.map(_.trigger)
+
+  private lazy val findDefaultCommand: Option[Command] = {
+    val default = _allCommands.filter(_.isDefaultCommand)
+    if default.size > 1 then {
+      println(
+        "Multiple commands injected with isDefaultCommand=true - using the first:"
+      )
+      default.foreach(c => println(c.getClass.getSimpleName))
+    }
+    default.headOption
+  }
+
+  @volatile
+  private var dropOne = true
+
+  final def main(args: Array[String]): Unit = {
+    val lzyApp = for {
+      cmd <- Lazy
+               .fromOption(args.headOption)
+               .map(commandMap.get)
+               .recover { case _: NoSuchElementException =>
+                 dropOne = false
+                 Lazy.fn(findDefaultCommand)
+               }
+               .someOrFail(
+                 new IllegalArgumentException(
+                   "Could not find command from argument, and no default command provided"
+                 )
+               )
+
+      result <- cmd.lazyAction(if dropOne then args.tail else args)
+    } yield result
+
+    lzyApp.runSync() match {
+      case Success(value)     => ()
+      case Failure(exception) => println(s"${exception.getMessage}")
+    }
+  }
 
 }
