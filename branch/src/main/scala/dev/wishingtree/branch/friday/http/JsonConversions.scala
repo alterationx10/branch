3332923e47da03eb5f47b9069fc5c7c01a8bf231
Mark Rudolph <mark@k8ty.app>
2024-12-28 13:58:02 -0500

    add some helpers to make Json Conversions

---
+++ branch/src/main/scala/dev/wishingtree/branch/friday/http/JsonConversions.scala
@@ -0,0 +1,26 @@
+package dev.wishingtree.branch.friday.http
+
+import dev.wishingtree.branch.friday.{JsonDecoder, JsonEncoder}
+
+object JsonConversions {
+
+  /** A Conversion[Array[Byte], A], useful for marshalling request bodies in
+    * spider
+    */
+  def convertFromBytes[A](using
+      decoder: JsonDecoder[A]
+  ): Conversion[Array[Byte], A] =
+    (bytes: Array[Byte]) =>
+      decoder
+        .decode(new String(bytes))
+        .getOrElse(throw new IllegalArgumentException("Failed to decode"))
+
+  /** A Conversion[A, Array[Byte]], useful for marshalling response bodies in
+    * spider
+    */
+  def convertToBytes[A](using
+      encoder: JsonEncoder[A]
+  ): Conversion[A, Array[Byte]] =
+    (a: A) => encoder.encode(a).toJsonString.getBytes
+
+}
---
+++ branch/src/main/scala/dev/wishingtree/branch/friday/http/JsonConversions.test.scala
@@ -0,0 +1,38 @@
+package dev.wishingtree.branch.friday.http
+
+import dev.wishingtree.branch.friday.JsonCodec
+import munit.*
+
+class JsonConversionsSpec extends FunSuite {
+
+  case class Person(name: String, age: Int) derives JsonCodec
+
+  test("JsonConversions.convertToBytes") {
+    given conv: Conversion[Person, Array[Byte]] =
+      JsonConversions.convertToBytes
+
+    val person = Person("Alice", 42)
+
+    assertEquals(
+      conv(person).toSeq,
+      person.toJson.toJsonString.getBytes.toSeq
+    )
+
+  }
+
+  test("JsonConversions.convertFromBytes") {
+    given conv: Conversion[Array[Byte], Person] =
+      JsonConversions.convertFromBytes
+
+    val bytes: Array[Byte] =
+      """{"name":"Alice","age":42}""".getBytes
+
+    val person =
+      Person("Alice", 42)
+
+    assertEquals(
+      conv(bytes),
+      person
+    )
+  }
+}
