3cd7c5b1787d23a0a928ea26142ef658324b0cce
Mark Rudolph <mark@k8ty.app>
2025-01-15 00:28:24 -0500

    try a different file name

--- branch/src/main/scala/dev/wishingtree/branch/mustachio/MustachioSectionsSpec.test.scala
+++ branch/src/main/scala/dev/wishingtree/branch/mustachio/MustachioSectionsSpec.test.scala
@@ -2,7 +2,7 @@ package dev.wishingtree.branch.mustachio
 
 class MustachioSectionsSpec extends MustacheSpecSuite {
 
-  specSuite("mustache/sections.json").tests
+  specSuite("mustache/sectionz.json").tests
     .foreach(runSpec)
 
 }
