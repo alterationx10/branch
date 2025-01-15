6a943aa0ade81a4e15e724f28b0af6cef0cf5eaf
Mark Rudolph <mark@k8ty.app>
2025-01-15 00:51:57 -0500

    move some models outside of suite trait

--- branch/src/main/scala/dev/wishingtree/branch/mustachio/MustacheSpecSuite.test.scala
+++ branch/src/main/scala/dev/wishingtree/branch/mustachio/MustacheSpecSuite.test.scala
@@ -5,65 +5,65 @@ import dev.wishingtree.branch.friday.{Json, JsonDecoder}
 import scala.io.Source
 import scala.util.{Try, Using}
 
+case class Spec(
+    name: String,
+    desc: String,
+    data: Json,
+    template: String,
+    expected: String,
+    partials: Json
+)
+
+object Spec {
+
+  // Because we're reading the tests from a file,
+  // it will escape the newlines, tabs, and carriage returns.
+  // We need to unescape them.
+  extension (s: String) {
+    def unescape: String =
+      s
+        .replaceAll("\\\\n", "\n")
+        .replaceAll("\\\\r", "\r")
+        .replaceAll("\\\\t", "\t")
+  }
+
+  given decoder: JsonDecoder[Spec] with {
+    def decode(json: Json): Try[Spec] = Try {
+      for {
+        name     <- json ? "name"
+        desc     <- json ? "desc"
+        data     <- json ? "data"
+        template <- json ? "template"
+        expected <- json ? "expected"
+        partials <- (json ? "partials").orElse(Some(Json.obj()))
+      } yield Spec(
+        name.strVal.unescape,
+        desc.strVal.unescape,
+        data,
+        template.strVal.unescape,
+        expected.strVal.unescape,
+        partials
+      )
+    }.map(_.get)
+  }
+
+}
+
+case class SpecSuite(tests: IndexedSeq[Spec])
+
+object SpecSuite {
+
+  given decoder: JsonDecoder[SpecSuite] with {
+    def decode(json: Json): Try[SpecSuite] = Try {
+      for {
+        tests <- json ? "tests"
+      } yield SpecSuite(tests.arrVal.map(Spec.decoder.decode(_).get))
+    }.map(_.get)
+  }
+
+}
+
 trait MustacheSpecSuite extends munit.FunSuite {
-
-  case class SpecSuite(tests: IndexedSeq[Spec])
-
-  object SpecSuite {
-
-    given decoder: JsonDecoder[SpecSuite] with {
-      def decode(json: Json): Try[SpecSuite] = Try {
-        for {
-          tests <- json ? "tests"
-        } yield SpecSuite(tests.arrVal.map(Spec.decoder.decode(_).get))
-      }.map(_.get)
-    }
-
-  }
-
-  case class Spec(
-      name: String,
-      desc: String,
-      data: Json,
-      template: String,
-      expected: String,
-      partials: Json
-  )
-
-  object Spec {
-
-    // Because we're reading the tests from a file,
-    // it will escape the newlines, tabs, and carriage returns.
-    // We need to unescape them.
-    extension (s: String) {
-      def unescape: String =
-        s
-          .replaceAll("\\\\n", "\n")
-          .replaceAll("\\\\r", "\r")
-          .replaceAll("\\\\t", "\t")
-    }
-
-    given decoder: JsonDecoder[Spec] with {
-      def decode(json: Json): Try[Spec] = Try {
-        for {
-          name     <- json ? "name"
-          desc     <- json ? "desc"
-          data     <- json ? "data"
-          template <- json ? "template"
-          expected <- json ? "expected"
-          partials <- (json ? "partials").orElse(Some(Json.obj()))
-        } yield Spec(
-          name.strVal.unescape,
-          desc.strVal.unescape,
-          data,
-          template.strVal.unescape,
-          expected.strVal.unescape,
-          partials
-        )
-      }.map(_.get)
-    }
-
-  }
 
   def runSpec(
       spec: Spec
