925f66f01fc0ce4fff2d09f56f7ef024bff90236
Mark Rudolph <mark@k8ty.app>
2025-01-25 16:10:39 -0500

    update scaladoc

--- branch/src/main/scala/dev/wishingtree/branch/veil/Veil.scala
+++ branch/src/main/scala/dev/wishingtree/branch/veil/Veil.scala
@@ -9,8 +9,8 @@ import scala.util.Try
   */
 object Veil {
 
-  /** The runtime environment, as based of the environment variable SCALA_ENV.
-    * If missing, or configured incorrectly, defaults to DEV.
+  /** The runtime environment, as based on the environment variable `SCALA_ENV`.
+    * If missing, or configured incorrectly, defaults to `DEV`.
     */
   val runtimeEnv: RuntimeEnv =
     System
@@ -20,12 +20,12 @@ object Veil {
       .flatMap(str => Try(RuntimeEnv.valueOf(str)).toOption)
       .getOrElse(RuntimeEnv.DEV)
 
-  /** Loads the env file into a Map, based on [[runtimeEnv]].
-    * {{{
-    * case DEV  => ".env"
-    * case TEST => ".env.test"
-    * case PROD => ".env.prod"
-    * }}}
+  /** Loads the env file into a `Map`, based on the [[runtimeEnv]].
+    *
+    * The file loaded depends on the runtime environment:
+    *   - `DEV` => ".env"
+    *   - `TEST` => ".env.test"
+    *   - `PROD` => ".env.prod"
     */
   private val dotEnv: Map[String, String] = {
     val envFile = this.runtimeEnv match {
@@ -49,16 +49,22 @@ object Veil {
 
   /** Utility method to strip prefixed quotes from a string, as might be common
     * in env files.
+    *
     * @param str
+    *   the string to strip quotes from
     * @return
+    *   the string without prefixed and suffixed quotes
     */
   private def stripQuotes(str: String): String =
     str.stripPrefix("\"").stripSuffix("\"")
 
   /** Get an environment variable by key. It first searches through variables
     * loaded from an env file, then through system variables.
+    *
     * @param key
+    *   the key of the environment variable
     * @return
+    *   an `Option` containing the value of the environment variable, if found
     */
   final def get(key: String): Option[String] =
     dotEnv.get(key).map(stripQuotes).orElse(System.getenv().asScala.get(key))
