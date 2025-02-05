51413203759a01c228955430777f25f70cedbbcc
Mark Rudolph <mark@k8ty.app>
2025-02-04 23:50:42 -0500

    Refactor SqlRuntime (#34)
    Moves SqlRuntime eval to Try from Future.fromTry

--- branch/src/main/scala/dev/wishingtree/branch/ursula/args/builtin/HelpFlag.scala
+++ branch/src/main/scala/dev/wishingtree/branch/ursula/args/builtin/HelpFlag.scala
@@ -4,8 +4,8 @@ import dev.wishingtree.branch.ursula.args.BooleanFlag
 
 /** A flag that triggers the display of help information.
   *
-  * This flag is a boolean flag, meaning it does not expect an argument.
-  * It can be triggered using either the long form "--help" or the short form "-h".
+  * This flag is a boolean flag, meaning it does not expect an argument. It can
+  * be triggered using either the long form "--help" or the short form "-h".
   */
 case object HelpFlag extends BooleanFlag {
   override val name: String        = "help"
