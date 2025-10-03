083543117202c36cff0d0ed6ee66172e67769da6
Mark Rudolph <mark@k8ty.app>
2025-10-02 22:46:55 -0400

    add covo agent test

---
+++ branch/src/test/scala/dev/alteration/branch/hollywood/ConversationalAgentSpec.scala
@@ -0,0 +1,24 @@
+package dev.alteration.branch.hollywood
+
+import dev.alteration.branch.testkit.fixtures.LlamaServerFixture
+
+class ConversationalAgentSpec extends LlamaServerFixture {
+
+  // Comment this in/out to run
+  override def munitIgnore: Boolean = true
+
+  // Set this to false if llama-server is already running
+  override val shouldStartLlamaServer: Boolean = true
+
+  test("ConversationalAgent should maintain conversation history") {
+    val agent     = ConversationalAgent()
+    val response1 = agent.chat("I have an orange cat named Whiskers.")
+    assert(response1.nonEmpty)
+    val response2 = agent.chat("What color was it?")
+    assert(response2.nonEmpty)
+    assert(
+      response2.toLowerCase.contains("orange")
+    )
+  }
+
+}
