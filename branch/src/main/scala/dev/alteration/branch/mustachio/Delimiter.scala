d0e035e311512d7c4da7524d620d6e568ac4f9ea
Mark Rudolph <mark@k8ty.app>
2025-10-07 23:16:40 -0400

    clean up unused delimeter methods

--- branch/src/main/scala/dev/alteration/branch/mustachio/Delimiter.scala
+++ branch/src/main/scala/dev/alteration/branch/mustachio/Delimiter.scala
@@ -1,35 +1,7 @@
 package dev.alteration.branch.mustachio
 
-import java.util.regex.Pattern
-
-private[mustachio] case class Delimiter(open: String, close: String) {
-
-  def closing(str: String): String =
-    s"$open/$str$close"
-
-  def section(str: String): String =
-    s"$open#$str$close"
-
-  def inversion(str: String): String =
-    s"$open^$str$close"
-
-  def partial(str: String): String =
-    s"$open>$str$close"
-
-  def comment(str: String): String =
-    s"$open!$str$close"
-
-  lazy val isDefault: Boolean =
-    Delimiter.default.open.equals(open) &&
-      Delimiter.default.close.equals(close)
-}
+private[mustachio] case class Delimiter(open: String, close: String)
 
 private[mustachio] object Delimiter {
   val default: Delimiter = Delimiter("{{", "}}")
-
-  def replaceDefaultWith(content: String, newDelimiter: Delimiter): String =
-    content
-      .replaceAll(Pattern.quote(default.open), newDelimiter.open)
-      .replaceAll(Pattern.quote(default.close), newDelimiter.close)
-
 }
