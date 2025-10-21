f2b027c478ecff03c161f16e4c77da3891dff7fd
Mark Rudolph <mark@k8ty.app>
2025-10-21 10:18:34 -0400

    touch up doc gen while we're at it

--- branch/src/main/scala/dev/alteration/branch/ursula/doc/FlagDoc.scala
+++ branch/src/main/scala/dev/alteration/branch/ursula/doc/FlagDoc.scala
@@ -6,28 +6,35 @@ import dev.alteration.branch.ursula.extensions.Extensions.*
 final case class FlagDoc(flag: Flag[?]) extends Documentation {
 
   override lazy val txt: String = {
-    val sb: StringBuilder = new StringBuilder()
+    val sb = new StringBuilder()
+
+    // Main flag line
     sb.append(s"${flag._sk}, ${flag._lk}")
     if (flag.expectsArgument) sb.append(" [arg]")
     sb.append(s"\t${flag.description}")
-    if (flag.required) sb.append(" [required")
+    if (flag.required) sb.append(" [required]")
     sb.newLine
+
+    // Dependencies
     if (flag.dependsOn.nonEmpty) {
-      sb.appendLine("Requires:")
+      sb.appendLine("Requires:".indented)
       flag.dependsOn.foreach { s =>
         s.foreach { f =>
-          sb.appendLine(s"\t${f._sk}, ${f._lk}")
+          sb.appendLine(s"${f._sk}, ${f._lk}".indented.indented)
         }
       }
     }
+
+    // Conflicts
     if (flag.exclusive.nonEmpty) {
-      sb.appendLine("Conflicts with:")
+      sb.appendLine("Conflicts with:".indented)
       flag.exclusive.foreach { s =>
         s.foreach { f =>
-          sb.appendLine(s"\t${f._sk}, ${f._lk}")
+          sb.appendLine(s"${f._sk}, ${f._lk}".indented.indented)
         }
       }
     }
+
     sb.toString()
   }
 
